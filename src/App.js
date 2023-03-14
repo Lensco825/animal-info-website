2ebe1adeb7e14b11a70b8e2beaf38adcfb1d6fb9
Lens <Lensdorsainvil711@outlook.com>
2023-03-13 20:10:15 -0400

    began making home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+
+var images = {
+  owl: 'https://images.unsplash.com/photo-1543549790-8b5f4a028cfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  bear: 'https://images.unsplash.com/photo-1568162603664-fcd658421851?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80',
+  dolphin: 'https://images.unsplash.com/photo-1570481662006-a3a1374699e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+  cat: 'https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav></nav>
+      <h1>Animal Info</h1>
+      <h2>Learn more about some of these animals!</h2>
+    <div className="AnimalSelection">
+     <div className="Owls"><img src={images.owl} alt='owl'></img></div>
+     <div className="grizzlyBears"><img src={images.bear} alt='bear'></img></div>
+     <div className="dolphins"><img src={images.dolphin} alt='dolphin'></img></div>
+     <div className="cats"><img src={images.cat} alt='cat'></img></div>
+    </div>
     </div>
   );
 }
