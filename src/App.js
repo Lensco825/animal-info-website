89fd54735706e1d476db00c859c08fc846771cb5
Lens <Lensdorsainvil711@outlook.com>
2023-03-21 18:11:28 -0400

    exported the other three content components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import {NAV} from './nav.js'
-import {bearContent} from "./info.js";
+import {bearContent, dolphinContent, catContent, owlContent} from "./info.js";
 var images = {
   owl: 'https://images.unsplash.com/photo-1543549790-8b5f4a028cfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
   bear: 'https://i.pinimg.com/736x/e6/a1/77/e6a17731d8ab49113dc4f1a61a2a0e8e.jpg',
@@ -26,7 +26,7 @@ function App() {
      <div className="cats"><img src={images.cat} alt='cat' className="coverImage"></img></div>
     </div>
     </main>
-    {bearContent()}
+    {owlContent()}
     </div>
   );
 }
