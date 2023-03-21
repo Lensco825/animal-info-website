0df71dd32ff75d3b0bf0e9ce5a4e9ae2bb0a1266
Lens <Lensdorsainvil711@outlook.com>
2023-03-21 17:00:03 -0400

    Made a content ctylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import {NAV} from './nav.js'
-import { InfoTemplate, Content } from "./info.js";
+import {bearContent} from "./info.js";
 var images = {
   owl: 'https://images.unsplash.com/photo-1543549790-8b5f4a028cfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
   bear: 'https://i.pinimg.com/736x/e6/a1/77/e6a17731d8ab49113dc4f1a61a2a0e8e.jpg',
@@ -19,14 +19,14 @@ function App() {
       <h2>Learn more about some of these animals!</h2>
     <div className="AnimalSelection">
      <div className="Owls">
-      <img src={images.owl} alt='owl'></img></div>
+      <img src={images.owl} alt='owl' className="coverImage"></img></div>
      <div className="grizzlyBears">
-      <img src={images.bear} alt='bear'></img></div>
-     <div className="dolphins"><img src={images.dolphin} alt='dolphin'></img></div>
-     <div className="cats"><img src={images.cat} alt='cat'></img></div>
+      <img src={images.bear} alt='bear' className="coverImage"></img></div>
+     <div className="dolphins"><img src={images.dolphin} alt='dolphin' className="coverImage"></img></div>
+     <div className="cats"><img src={images.cat} alt='cat' className="coverImage"></img></div>
     </div>
     </main>
-    
+    {bearContent()}
     </div>
   );
 }
