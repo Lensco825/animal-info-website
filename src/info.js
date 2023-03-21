5597cf80c3e5b37f2bde9b538d76dc4dde5309c1
Lens <Lensdorsainvil711@outlook.com>
2023-03-21 18:02:02 -0400

    Stylized all animal content

--- src/info.js
+++ src/info.js
@@ -22,68 +22,53 @@ export function bearContent() {
 
 export function dolphinContent() {
     return (
-    <main>
-        <h1>The ballorenas of the Seas!</h1>
-        <p>Mostly known for their acrobatic tricks out of the water, the dolphin is an amazing site to see in the ocean! They're marine mammals that belong to the family Delphinidae. They are known for their intelligence, social behavior, and playful nature. In this research report, we will explore the fascinating world of dolphins.</p>
-        <br></br>
-        <h2>Physical Characteristics</h2>
-        <p>Dolphins are known for their sleek bodies, which are designed for efficient swimming. They have a streamlined shape, with a dorsal fin on their back and two pectoral fins on their sides. Dolphins can range in size from 4 to 30 feet, depending on the species. They use echolocation to navigate their environment and communicate with other dolphins.</p>
-        <img src="" alt="dolphin flipping"></img>
-        <br></br>
-        <h2>Habitat and Diet</h2>
-        <p>Dolphins are found in oceans and rivers all over the world. They live in groups called pods, which can consist of up to several hundred individuals. Dolphins are carnivorous and feed on a variety of fish, squid, and crustaceans. They are known for their cooperative hunting behavior, where they work together to herd fish into a tight group before feeding.</p>
-        <br></br>
-        <h2>Social Behavior</h2>
-        <p>Dolphins are highly social animals and have complex social structures. They communicate with each other using a variety of clicks, whistles, and body language. Dolphins are also known for their playful behavior, often jumping out of the water and riding the waves of boats. They have been observed engaging in acts of altruism, such as protecting injured members of their pod.</p>
-        <br></br>
-        <h2>Conservation Status</h2>
-        <p>Many species of dolphins are currently threatened or endangered due to human activities such as overfishing, pollution, and habitat destruction. It is important to protect and conserve these magnificent creatures to ensure their survival for future generations.</p>
-    </main>
+    <div className="information">
+        <h2 style={{backgroundImage: 'url(https://images.unsplash.com/photo-1607153333879-c174d265f1d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80)', padding: '100px 0 100px 0', borderRadius: '10px', width: '60%', color: 'white'}}>The ballorenas of the Seas!</h2>
+        <p className="text">Mostly known for their acrobatic tricks out of the water, the dolphin is an amazing site to see in the ocean! They're marine mammals that belong to the family Delphinidae. They are known for their intelligence, social behavior, and playful nature. In this research report, we will explore the fascinating world of dolphins.</p>
+        <h3>Physical Characteristics</h3>
+        <p className="text">Dolphins are known for their sleek bodies, which are designed for efficient swimming. They have a streamlined shape, with a dorsal fin on their back and two pectoral fins on their sides. Dolphins can range in size from 4 to 30 feet, depending on the species. They use echolocation to navigate their environment and communicate with other dolphins.</p>
+        <h3>Habitat and Diet</h3>
+        <p className="text">Dolphins are found in oceans and rivers all over the world. They live in groups called pods, which can consist of up to several hundred individuals. Dolphins are carnivorous and feed on a variety of fish, squid, and crustaceans. They are known for their cooperative hunting behavior, where they work together to herd fish into a tight group before feeding.</p>
+        <h3>Social Behavior</h3>
+        <p className="text">Dolphins are highly social animals and have complex social structures. They communicate with each other using a variety of clicks, whistles, and body language. Dolphins are also known for their playful behavior, often jumping out of the water and riding the waves of boats. They have been observed engaging in acts of altruism, such as protecting injured members of their pod.</p>
+        <h3>Conservation Status</h3>
+        <p className="text">Many species of dolphins are currently threatened or endangered due to human activities such as overfishing, pollution, and habitat destruction. It is important to protect and conserve these magnificent creatures to ensure their survival for future generations.</p>
+    </div>
     )
 }
 
 
 export function owlContent() {
-    <main>
-    <h1>The silent assasin</h1>
-    <p>Owls are a unique species of birds that have fascinated humans for centuries. These birds are known for their distinct features such as their large eyes, silent flight, and sharp talons. In this research report, we will delve into the fascinating world of owls and explore some of their most interesting characteristics.</p>
-    <br></br>
-    <h2>Physical Characteristics</h2>
-    <p>Owls come in various sizes, with the smallest species being the Elf Owl, which is about 5 inches tall, while the largest species is the Eurasian Eagle Owl, which can grow up to 30 inches tall. They have large heads with a flat face, and their eyes are forward-facing, which gives them excellent depth perception. Their wings are adapted for silent flight, which allows them to sneak up on their prey undetected. Owls also have sharp talons that are used to catch and kill their prey.</p>
-    <img src="" alt="dolphin flipping"></img>
-    <br></br>
-    <h2>Behavior</h2>
-    <p>Owls are primarily nocturnal creatures, which means they are active during the night. They are solitary animals and prefer to hunt alone, but some species will hunt in pairs. Owls are also known for their incredible eyesight, which is several times better than humans. They use their eyesight to locate their prey, which can be small mammals, birds, and insects. Owls are also very territorial and will defend their territory fiercely.</p>
-    <br></br>
-    <h2>Habitat</h2>
-    <p>Owls can be found all over the world, except for Antarctica. They are found in a variety of habitats, from forests to deserts. Different species of owls have adapted to live in different habitats. For example, the Snowy Owl lives in the Arctic tundra, while the Burrowing Owl lives in the grasslands of North and South America.</p>
-    <br></br>
-    <h2>Threats to Owls</h2>
-    <p>Unfortunately, owls face many threats in the wild. One of the biggest threats is habitat loss, as humans continue to encroach on their natural habitats. Owls are also hunted for their feathers and body parts, which are used in traditional medicines. Climate change is also affecting owls, as their habitats are changing, and their prey is becoming scarce.</p>
-</main>
+    <div className="information">
+    <h2 style={{backgroundImage: 'url(https://images.unsplash.com/photo-1526409049865-5b0b188372f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80)', padding: '100px 0 100px 0', borderRadius: '10px', width: '60%', color: 'white'}}>The silent assasin</h2>
+    <p className="text">Owls are a unique species of birds that have fascinated humans for centuries. These birds are known for their distinct features such as their large eyes, silent flight, and sharp talons. In this research report, we will delve into the fascinating world of owls and explore some of their most interesting characteristics.</p>
+    <h3>Physical Characteristics</h3>
+    <p className="text">Owls come in various sizes, with the smallest species being the Elf Owl, which is about 5 inches tall, while the largest species is the Eurasian Eagle Owl, which can grow up to 30 inches tall. They have large heads with a flat face, and their eyes are forward-facing, which gives them excellent depth perception. Their wings are adapted for silent flight, which allows them to sneak up on their prey undetected. Owls also have sharp talons that are used to catch and kill their prey.</p>
+    <h3>Behavior</h3>
+    <p className="text">Owls are primarily nocturnal creatures, which means they are active during the night. They are solitary animals and prefer to hunt alone, but some species will hunt in pairs. Owls are also known for their incredible eyesight, which is several times better than humans. They use their eyesight to locate their prey, which can be small mammals, birds, and insects. Owls are also very territorial and will defend their territory fiercely.</p>
+    <h3>Habitat</h3>
+    <p className="text">Owls can be found all over the world, except for Antarctica. They are found in a variety of habitats, from forests to deserts. Different species of owls have adapted to live in different habitats. For example, the Snowy Owl lives in the Arctic tundra, while the Burrowing Owl lives in the grasslands of North and South America.</p>
+    <h3>Threats to Owls</h3>
+    <p className="text">Unfortunately, owls face many threats in the wild. One of the biggest threats is habitat loss, as humans continue to encroach on their natural habitats. Owls are also hunted for their feathers and body parts, which are used in traditional medicines. Climate change is also affecting owls, as their habitats are changing, and their prey is becoming scarce.</p>
+</div>
 }
 
 
 export function catContent() {
     return (
-        <main>
-        <h1>The Fascinating Felines</h1>
-        <p>Cats are one of the most popular pets in the world, and with good reason. They are fascinating creatures that have been around for thousands of years. In this research report, we will explore five different aspects of cats that make them truly unique and special.</p>
-        <br></br>
-        <h2>Physical Characteristics</h2>
-        <p>Cats are known for their soft fur, sharp claws, and their ability to retract their claws. Unlike dogs, cats are obligate carnivores, which means that they need meat to survive. They have excellent hearing and vision, and their whiskers are highly sensitive, helping them navigate their environment. Cats are also known for their ability to jump great distances, as well as their agility and flexibility.</p>
-        <img src="" alt="dolphin flipping"></img>
-        <br></br>
-        <h2>Behavior</h2>
-        <p>Cats are independent animals and are often seen as aloof. However, they are also very social creatures and form strong bonds with their owners. They are expert hunters and are known for their ability to catch mice and other small rodents. They are also playful and enjoy toys, especially those that mimic prey. In addition, cats are known for their grooming habits and spend a significant amount of time cleaning their fur.</p>
-        <br></br>
-        <h2>History</h2>
-        <p>Cats have a long and varied history. They were first domesticated in ancient Egypt over 4,000 years ago and were revered as sacred animals. They were often depicted in art and were even mummified alongside their owners. Cats also played a role in the Middle Ages, where they were used to control the rodent population in cities. Today, cats are found on every continent except Antarctica and are beloved pets in households around the world.</p>
-        <br></br>
-        <h2>Conservation Status</h2>
-        <p>Many species of dolphins are currently threatened or endangered due to human activities such as overfishing, pollution, and habitat destruction. It is important to protect and conserve these magnificent creatures to ensure their survival for future generations.</p>
-        <h2>Breeds</h2>
-        <p>There are many different breeds of cats, each with their own unique characteristics. Some of the most popular breeds include the Siamese, Persian, Maine Coon, and British Shorthair. Each breed has its own personality and physical characteristics, and it is important for potential owners to research different breeds to find one that fits their lifestyle.</p>
-    </main>
+        <div className="information">
+        <h2 style={{backgroundImage: 'url(https://images.unsplash.com/photo-1568043210943-0e8aac4b9734?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)', padding: '100px 0 100px 0', borderRadius: '10px', width: '60%', color: 'white'}}>The Fascinating Felines</h2 >
+        <p className="text">Cats are one of the most popular pets in the world, and with good reason. They are fascinating creatures that have been around for thousands of years. In this research report, we will explore five different aspects of cats that make them truly unique and special.</p>
+        <h3>Physical Characteristics</h3>
+        <p className="text">Cats are known for their soft fur, sharp claws, and their ability to retract their claws. Unlike dogs, cats are obligate carnivores, which means that they need meat to survive. They have excellent hearing and vision, and their whiskers are highly sensitive, helping them navigate their environment. Cats are also known for their ability to jump great distances, as well as their agility and flexibility.</p>
+        <h3>Behavior</h3>
+        <p className="text">Cats are independent animals and are often seen as aloof. However, they are also very social creatures and form strong bonds with their owners. They are expert hunters and are known for their ability to catch mice and other small rodents. They are also playful and enjoy toys, especially those that mimic prey. In addition, cats are known for their grooming habits and spend a significant amount of time cleaning their fur.</p>
+        <h3>History</h3>
+        <p className="text">Cats have a long and varied history. They were first domesticated in ancient Egypt over 4,000 years ago and were revered as sacred animals. They were often depicted in art and were even mummified alongside their owners. Cats also played a role in the Middle Ages, where they were used to control the rodent population in cities. Today, cats are found on every continent except Antarctica and are beloved pets in households around the world.</p>
+        <h3>Conservation Status</h3>
+        <p className="text">Many species of dolphins are currently threatened or endangered due to human activities such as overfishing, pollution, and habitat destruction. It is important to protect and conserve these magnificent creatures to ensure their survival for future generations.</p>
+        <h3>Breeds</h3>
+        <p className="text">There are many different breeds of cats, each with their own unique characteristics. Some of the most popular breeds include the Siamese, Persian, Maine Coon, and British Shorthair. Each breed has its own personality and physical characteristics, and it is important for potential owners to research different breeds to find one that fits their lifestyle.</p>
+    </div>
     )
 }
