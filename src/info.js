33400980082457c288107c87629d6fde3d10ebdd
Lens <Lensdorsainvil711@outlook.com>
2023-03-21 17:05:46 -0400

    shortened the size of the headers

--- src/info.js
+++ src/info.js
@@ -6,20 +6,17 @@ export function bearContent() {
 
     return (
         <main>
-        <h1>Bears: A Fascinating Species</h1>
+        <h2>Bears: A Fascinating Species</h2>
         <p>Bears are a group of mammals that have captured the hearts and imaginations of people all over the world. They are known for their impressive size, strength, and intelligence. In this research report, we will explore the different species of bears, their habitats, and some of the challenges they face in the wild.</p>
-        <br></br>
-        <h2>Species of Bears</h2>
+        <h3>Species of Bears</h3>
         <p>There are eight different species of bears in the world. These include the American black bear, the brown bear, the polar bear, the Asiatic black bear, the sloth bear, the sun bear, the spectacled bear, and the giant panda. Each species has its unique characteristics, such as the polar bear's ability to swim long distances or the giant panda's diet of bamboo.</p>
         <img src="https://images.unsplash.com/photo-1593946460607-d1570da6268f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt='Polar Bear'></img>
-        <br></br>
-        <h1>Habitats</h1>
-        <p>Bears can be found in a variety of habitats, from dense forests to Arctic ice caps. Many species of bears are native to North America, while others can be found in Asia, Europe, and South America. Some species, such as the polar bear, are adapted to living in harsh environments, while others, like the sun bear, thrive in tropical climates.</p>
-        <p>However, some don't stay in one specefic continent, the polar for example, mostly lives in the Artic but is also seen lower north in Canada, Russia, and Alaska. So as long as the climate and enviorment fits them they will be found there.</p>
+        <h3>Habitats</h3>
+        <p>Bears can be found in a variety of habitats, from dense forests to Arctic ice caps. Many species of bears are native to North America, while others can be found in Asia, Europe, and South America. Some species, such as the polar bear, are adapted to living in harsh environments, while others, like the sun bear, thrive in tropical climates. However, some don't stay in one specefic continent, the polar for example, mostly lives in the Artic but is also seen lower north in Canada, Russia, and Alaska. So as long as the climate and enviorment fits them they will be found there.</p>
         <img src="https://images.unsplash.com/photo-1592186534355-ae2784d4c3da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt='huh'></img>
-        <h2>Challenges</h2>
+        <h3>Challenges</h3>
         <p>Bears face a variety of challenges in the wild. Habitat loss due to human activities, such as deforestation and urbanization, is a significant threat to many bear species. Climate change is also affecting bears, especially those that rely on sea ice, such as the polar bear. Additionally, bears are often hunted for their fur, meat, and body parts, which are used in traditional medicine.</p>
-        <h2>Conservation Efforts</h2>
+        <h3>Conservation Efforts</h3>
         <p>To protect bears and their habitats, conservation efforts are underway around the world. These efforts include habitat restoration, captive breeding programs, and anti-poaching initiatives. Additionally, many organizations work to educate the public about the importance of bears and their role in the ecosystem.</p>
         <img src="https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt='huh'></img>
         </main>
