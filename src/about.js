4384c28ab4a3fb2d0a81dcaf8d0fb760731f7880
Lens <Lensdorsainvil711@outlook.com>
2023-03-23 20:33:38 -0400

    Created about content component

---
+++ src/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function About() {
+    return (
+        <div>
+            <h1>About This Website</h1>
+            <p>This website was made by Lens Dorsainvil, AKA Lensco, as a project to practice and learn how 
+            to use React. By the time you see this i've posted a <a href="https://dev.to/lensco825">Blog</a> that contains an overview on how i made it. 
+            All of the images used hear were on Unsplash, with my icons from ion-icon. I'm a 13 year old front end developer hoping to become full stack by high
+            school and maybe get a high paying job as a web developer to pay for college! If you have any questions or if you just want to say
+            something to me email me at lensdorsainvil711@outlook.com.</p>
+        </div>
+    )
+}
